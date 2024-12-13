74a35d036a43695d73838734b1c51d9e9acaef99
Frederik Rommel <frederik.rommel@webidea24.de>
2024-12-13 14:34:35 +0100

    payment-gateway: implement cancel

---
+++ src/Gateway/RequestBuilder/CancelOrderRequestBuilder.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * (c) WEBiDEA
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tilta\Payment\Gateway\RequestBuilder;
+
+use Magento\Payment\Gateway\Helper\SubjectReader;
+use Magento\Payment\Gateway\Request\BuilderInterface;
+use Tilta\Sdk\Model\Request\Order\CancelOrderRequestModel;
+
+class CancelOrderRequestBuilder implements BuilderInterface
+{
+    public function build(array $buildSubject): array
+    {
+        $payment = SubjectReader::readPayment($buildSubject);
+
+        return [
+            'request_model' => new CancelOrderRequestModel($payment->getOrder()->getOrderIncrementId()),
+        ];
+    }
+}
