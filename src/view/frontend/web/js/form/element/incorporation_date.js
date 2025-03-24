e4ff64b86368c8db7176c4ad002663e5ab4f34a3
Frederik Rommel <frederik.rommel@webidea24.de>
2025-03-24 22:17:15 +0100

    TILMAG-15: incorporatedAt: remove day from frontend

--- src/view/frontend/web/js/form/element/incorporation_date.js
+++ src/view/frontend/web/js/form/element/incorporation_date.js
@@ -57,6 +57,10 @@ define([
                 };
             }
 
+            if (!this.valueDay()) {
+                this.valueDay(1);
+            }
+
             const value = this.value();
             const isValid = value ? value.match(/^\d{4}-\d{2}-\d{2}$/) : false
 
